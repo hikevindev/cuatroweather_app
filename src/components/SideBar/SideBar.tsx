e9f9c1c1c2d8d5aee6bc4ef0ddeec68935a4d277
unknown <devkevinrivera@gmail.com>
2024-11-21 11:59:01 +0100

    feat: Add search field

--- src/components/SideBar/SideBar.tsx
+++ src/components/SideBar/SideBar.tsx
@@ -3,12 +3,14 @@ import { useTranslation } from 'react-i18next';
 import { useEffect } from 'react';
 import { fetchWeather } from '../../redux/slices/weatherSlice';
 import { AppDispatch } from '../../redux/store';
+import React, { useState } from 'react';
 
 import './SideBar.scss';
 
 export const SideBar = () => {
   const dispatch = useDispatch<AppDispatch>();
   const { t } = useTranslation();
+  const [locationSearch, setLocationSearch] = useState('');
 
   useEffect(() => {
     dispatch(fetchWeather('Londres'));
@@ -20,6 +22,19 @@ export const SideBar = () => {
 
   return (
     <aside className="sidebar">
+      <div className="sidebar__search">
+        <input
+          value={locationSearch}
+          onChange={(ev) => {
+            setLocationSearch(ev.target.value);
+          }}
+          type="text"
+          placeholder="Ciudad"
+        />
+        <button onClick={() => changeWeatherCity(locationSearch)}>
+          {'->'}
+        </button>
+      </div>
       <div>
         <nav>
           <p
@@ -39,6 +54,12 @@ export const SideBar = () => {
             onClick={() => changeWeatherCity('Singapur')}
           >
             Singapur
+          </p>
+          <p
+            className="sidebar__option"
+            onClick={() => changeWeatherCity('Madrid')}
+          >
+            Madrid
           </p>
         </nav>
       </div>
