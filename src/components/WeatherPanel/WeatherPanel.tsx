5905a091462b15185f3cdf2844dfc7c2ab616bfc
unknown <devkevinrivera@gmail.com>
2024-11-25 15:17:05 +0100

    feat: Update Weather Panel

--- src/components/WeatherPanel/WeatherPanel.tsx
+++ src/components/WeatherPanel/WeatherPanel.tsx
@@ -13,7 +13,47 @@ export const WeatherPanel = () => {
     <section className="weather-panel">
       <div className="weather-panel__values">
         <div className="weather-panel__value-container">
-          <div>
+          <div className="weather-panel__header">
+            <h2>CURRENT WEATHER</h2>
+            <span>2:25 PM</span>
+          </div>
+          <div className="weather-panel__main">
+            <div>
+              <img
+                src={`https://openweathermap.org/img/wn/${data?.weather?.icon}@2x.png`}
+                alt="Icono del clima"
+                className="weather-panel__media"
+              />
+              <div>
+                <p className="current-temp">
+                  {data?.temp}º<span>C</span>
+                </p>
+                <span>
+                  {data?.tempMax}º/{data?.tempMin}º
+                </span>
+                <p className="weather-panel__description">
+                  {data?.weather?.description}
+                </p>
+              </div>
+            </div>
+            <div>
+              <div className="weather-panel__dates">
+                <p>
+                  Ciudad <span>{data?.city}</span>
+                </p>
+                <p>
+                  humedad <span>{data?.humidity}</span>
+                </p>
+                <p>
+                  Sensación termica<span>{data?.tempFeels}</span>
+                </p>
+                <p>
+                  Velocidad del viento<span>{data?.windVelocity}</span>
+                </p>
+              </div>
+            </div>
+          </div>
+          {/* <div>
             <img
               src={`https://openweathermap.org/img/wn/${data?.weather?.icon}@2x.png`}
               alt="Icono del clima"
@@ -24,14 +64,12 @@ export const WeatherPanel = () => {
             <p className="weather-panel__date">
               {data?.city}, {todayString}
             </p>
-            <p className="weather-panel__description">
-              {data?.weather?.description}
-            </p>
+            
             <p className="weather-panel__temp">{data?.temp}º</p>
             <p>
               {data?.tempMax}º/{data?.tempMin}º
             </p>
-          </div>
+          </div> */}
         </div>
       </div>
     </section>
